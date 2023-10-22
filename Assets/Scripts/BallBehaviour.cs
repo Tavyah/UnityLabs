2d671c8c4bfe3b59fb059ea31689b37458166375
Tavyah <tavylizia@gmail.com>
2023-10-22 10:28:31 +0200

    Updated BallBehaviour script
    Added fields and modified script

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
+    [SerializeField] private float XRotation = 0f;
+    [SerializeField] private float YRotation = 1f;
+    [SerializeField] private float ZRotation = 0f;
+    [SerializeField] private float DegreesPerSecond = 180f;
+
     private void Update()
     {
-        transform.Rotate(Vector3.up, 180 * Time.deltaTime);
+        Vector3 axis = new Vector3 (XRotation, YRotation, ZRotation);
+        transform.Rotate(axis, DegreesPerSecond * Time.deltaTime);
     }
 }
